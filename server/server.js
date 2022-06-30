d1e88258b9a53770f8a2568e3bab10cf7f672278
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2022-06-30 17:11:45 +0700

    Feature: use cors & json parser

--- server/server.js
+++ server/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+require("dotenv").config();
+
+require("");
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
