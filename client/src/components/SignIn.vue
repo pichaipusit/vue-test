e99d01456d05caaf5c42886d9620b8292d646998
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2022-06-30 23:25:57 +0700

    Feature: add email input and validation

--- client/src/components/SignIn.vue
+++ client/src/components/SignIn.vue
@@ -1,10 +1,37 @@
 <template>
-  <div></div>
+  <div>
+
+    <el-form :model="dynamicValidateForm" ref="dynamicValidateForm" label-width="120px" class="demo-dynamic">
+  <el-form-item
+    prop="email"
+    label="Email"
+    :rules="[
+      { required: true, message: 'Please input email address', trigger: 'blur' },
+      { type: 'email', message: 'Please input correct email address', trigger: ['blur', 'change'] }
+    ]"
+  >
+    <el-input v-model="dynamicValidateForm.email"></el-input>
+  </el-form-item>
+
+  </div>
 </template>
 
 <script>
 export default {
   name: "SignIn",
+  
+      data() {
+      return {
+        dynamicValidateForm: {
+          domains: [{
+            key: 1,
+            value: ''
+          }],
+          email: ''
+        }
+      };
+    },
+
 };
 </script>
 
